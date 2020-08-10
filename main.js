bcfd6428e00d63d1cb2b6f5042941d4affe75f38
xtakoyakii <50556930+xtakoyakii@users.noreply.github.com>
2020-08-10 08:35:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-function detectmob() {
-  if(window.innerWidth <= 800 || window.innerHeight <= 600) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-if (detectmob()){
-top.location.href="mobile.html";
+if (screen.width <= 699) {
+  document.location = "/mobile.html";
 }
 
 var TxtType = function(el, toRotate, period) {
